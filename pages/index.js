28dd9fc6856f42c3c402f13c2900805d1d1a03aa
Rafael <rafael_oliveira@gmx.com>
2021-03-22 19:57:37 -0300

    Update fetch to netlify domain

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,12 @@ export default function Home(props) {
   return (
     <Quote quotes={props.quotes} />
   )
-  
+
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch("http://localhost:3000/api/quotes");
+  //const res = await fetch("http://localhost:3000/api/quotes");
+  const res = await fetch("https://elastic-agnesi-c2d665.netlify.app/api/quotes");
   const quotes = await res.json();
 
   return {
